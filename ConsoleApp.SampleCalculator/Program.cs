6b3f4a3509ea358dd6c0b2621e6769ce0b97f20f
Jesus Salinas <34974398+jesus101589@users.noreply.github.com>
2025-07-13 16:11:00 -0500

    Simple calculator complete

--- ConsoleApp.SampleCalculator/Program.cs
+++ ConsoleApp.SampleCalculator/Program.cs
@@ -1,4 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
+
 // Welcome to the Sample Calculator Console Application!
 Console.WriteLine("Welcome to the Sample Calculator Console Application!");
 
@@ -9,8 +10,57 @@ int num1 = Convert.ToInt32(Console.ReadLine());
 Console.Write("Please enter the second number: ");
 int num2 = Convert.ToInt32(Console.ReadLine());
 
-// Show calcution options
+// Show calculator options / Show Menu
+Console.WriteLine("Please select an operation");
+Console.WriteLine("1. Addition");
+Console.WriteLine("2. Subtraction");
+Console.WriteLine("3. Multiplication");
+Console.WriteLine("4. Division");
+int choice = Convert.ToInt32(Console.ReadLine());
+
 
 // Decide which operation is needed based on selected option
+//if(choice == 1)
+////{
+////    /* Addition */
+////}
+////else if(choice == 2)
+////{
+////    /* Subtraction */
+////}
+////else if(choice == 3)
+////{
+////    /* Multiplication */
+////}
+////else if(choice == 4)
+////{
+////    /* Division */
+////}
+////else
+////{
+////    Console.WriteLine("Invalid choice. Please select a valid operation.");
+////}
 
+/* Switch statement to handle operations */
+int answer = 0;
+switch (choice)
+{
+    case 1:
+        answer = num1 + num2;
+        break;
+    case 2:
+        answer = num1 - num2;
+        break;
+    case 3:
+        answer = num1 * num2;
+        break;
+    case 4:
+        answer = num1 / num2;
+        break;
+    default:
+        Console.WriteLine("Invalid choice. Please select a valid operation.");
+        break;
+}
 // Print output
+
+Console.WriteLine($"The result is: {answer}");
