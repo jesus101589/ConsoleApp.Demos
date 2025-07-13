4e63d2dec044c4e1c67e4395428b1c8a9786bf09
Jesus Salinas <34974398+jesus101589@users.noreply.github.com>
2025-07-13 13:44:12 -0500

    Start of sampe calculator

---
+++ ConsoleApp.SampleCalculator/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Welcome to the Sample Calculator Console Application!
+Console.WriteLine("Welcome to the Sample Calculator Console Application!");
+
+// Prompt the user for input
+Console.Write("Please enter the first number: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Please enter the second number: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+// Show calcution options
+
+// Decide which operation is needed based on selected option
+
+// Print output
