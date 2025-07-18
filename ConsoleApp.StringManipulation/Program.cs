6f47cc565a0991cfbbc10ccc39734f1b42a2bb33
Jesus Salinas <34974398+jesus101589@users.noreply.github.com>
2025-07-18 13:02:59 -0500

    String and DateTime manipulations

---
+++ ConsoleApp.StringManipulation/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+// Initialize with a regular string literal.
+string s1 = "This a string literal."; // preferred way to initialize a string
+String s2 = "This is another string literal.";
+
+// Declare without initialization. (possible null exception)
