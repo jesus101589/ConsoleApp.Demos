4f1d75bcaefb1a7586b624065d8e477069aa2c1d
Jesus Salinas <34974398+jesus101589@users.noreply.github.com>
2025-07-19 13:01:40 -0500

    few more examples before work.

--- ConsoleApp.StringManipulation/Program.cs
+++ ConsoleApp.StringManipulation/Program.cs
@@ -2,7 +2,26 @@
 Console.WriteLine("Hello, World!");
 
 // Initialize with a regular string literal.
-string s1 = "This a string literal."; // preferred way to initialize a string
+string s1 = "This a string literal."; // preferred way to initialize a string, using lower case 'string'
 String s2 = "This is another string literal.";
 
 // Declare without initialization. (possible null exception)
+string s3;
+
+// Initialize to null. (possible null exception)
+string? s4 = null;
+
+// Initialize as an empty string.
+string s5 = string.Empty; // "" (this is a safer way to initialize an empty string)
+string s6 = "";
+
+// Escape sequences and characters.
+// She said, "I have your phone"
+string sentence = "She said, \" I have your phone\" \r\n This is a new line.";
+
+// Verbatim string literal.
+string oldPath = "C:\\program Files\\programfolder\\"; //escaped backslash
+string newPath = @"C:\program Files\programfolder\"; // verbatim string literal, no need to escape backslash
+
+
+// Use a constant string to prevent modification to a string.
