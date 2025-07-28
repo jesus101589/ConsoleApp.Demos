79e13b968b17f1f8a7fe46a47c20009f7feca39f
Jesus Salinas <34974398+jesus101589@users.noreply.github.com>
2025-07-27 20:55:32 -0500

    Short method practice before bed. Will spend a full day tomorrow since I am off work!

--- ConsoleApp.Methods/Program.cs
+++ ConsoleApp.Methods/Program.cs
@@ -1,1 +1,28 @@
 ﻿Console.WriteLine("************ - Methods - ************");
+
+// Void methods - completes a task but does not return a value
+void PrintName()
+{ 
+    // Method code
+    Console.WriteLine("Jesus Salinas: Software Engineer");
+}
+
+// Value returning methods - returns a value after an operation
+int GetFiveYearsAgo()
+{
+    int year = DateTime.Now.AddYears(-5).Year;
+    return year;
+}
+
+
+// Methods with parameters
+
+// Methods with optional parameters
+
+// Methods with nullable parameters
+
+
+/* Function Calls */
+PrintName();                                              
+int fiveYearsAgo = GetFiveYearsAgo();                       
+Console.WriteLine($"Five Years Ago was: {fiveYearsAgo}");                      
