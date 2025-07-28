c647dead594998a0a093ce4f9c37afdda13b3a55
Jesus Salinas <34974398+jesus101589@users.noreply.github.com>
2025-07-28 14:16:01 -0500

    Finshed Arrays and Practing Lists now.

--- ConsoleApp.Arrays/Program.cs
+++ ConsoleApp.Arrays/Program.cs
@@ -6,17 +6,39 @@ int[] grades = new int[5];
 // IF n is the size of the array, then your array will have addresses from 0 to n-1.
 
 // Add values to the Fixed Size Array
-grades[0] = 45;
-grades[1] = 25;
-grades[2] = 38;
-grades[3] = 45;
-grades[4] = 54;
+//grades[0] = 45;
+//grades[1] = 25;
+//grades[2] = 38;
+//grades[3] = 45;
+//grades[4] = 54;
+
+for (int i = 0; i < grades.Length; i++)
+{
+    Console.Write("Enter Grade: ");
+    grades[i] = Convert.ToInt32(Console.ReadLine());
+}
 
 // Print values in the Fixed Size Array
-
+Console.WriteLine("The Grades you have entered are: ");
+for (int i = 0; i < grades.Length; i++)
+{ 
+    Console.WriteLine(grades[i]);
+}
 
 // Declare Variable sized Array
+string[] studentNames = { "Jesus", "Kelly", "Smokey" };
 
 // Add values to the Variable sized Array
 
+for (int i = 0; i < studentNames.Length; i++)
+{
+    Console.Write("Enter Student Name: ");
+    studentNames[i] = Console.ReadLine();
+}
+
 // Print values in the Variable sized Array
+Console.WriteLine("The student names you have entered are: ");
+for (int i = 0; i < studentNames.Length; i++)
+{
+    Console.WriteLine(studentNames[i]);
+}
