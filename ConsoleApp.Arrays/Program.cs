14d02c3affb0575c56fbf22af230b0866529d55f
Jesus Salinas <34974398+jesus101589@users.noreply.github.com>
2025-07-28 13:22:29 -0500

    Array practice now

---
+++ ConsoleApp.Arrays/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.WriteLine("********** - Arrays - **********");
+
+// Declare Fixed Size Array
+int[] grades = new int[5];
+// Here we defined 5 addresses/indexes in memory for the grades array 0, 1, 2, 3, and 4.
+// IF n is the size of the array, then your array will have addresses from 0 to n-1.
+
+// Add values to the Fixed Size Array
+grades[0] = 45;
+grades[1] = 25;
+grades[2] = 38;
+grades[3] = 45;
+grades[4] = 54;
+
+// Print values in the Fixed Size Array
+
+
+// Declare Variable sized Array
+
+// Add values to the Variable sized Array
+
+// Print values in the Variable sized Array
