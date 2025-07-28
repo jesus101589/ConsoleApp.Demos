6ab88126f5968cd39e8f2b566d75af14035fbdef
Jesus Salinas <34974398+jesus101589@users.noreply.github.com>
2025-07-28 11:24:26 -0500

    Finished Methods Practice

--- ConsoleApp.Methods/Program.cs
+++ ConsoleApp.Methods/Program.cs
@@ -27,13 +27,37 @@ int GetYearDifferenceWithParams(int year)
     int yearDifference = DateTime.Now.Year - year;
     return yearDifference;
 }
-// Methods with optional parameters
-
+// Methods with optional parameters - parameter is not required..also has a default value
+int GetFutureOrPastYear(int numberOfYears = 0)
+{
+    var year = DateTime.Now.AddYears(numberOfYears).Year;
+    return year;
+}
 // Methods with nullable parameters
+void PrintNameNullableParam(string? name, int? count = 0) 
+{
+    /*
+    if(string.IsNullOrEmpty(name))
+    {
+       name = "Default Name";
+    }
+    if(!count.HasValue)
+    {
+        count = 1;
+    }
+    */
 
+    name ??= "Default Name"; // If name is null, assign "Default Name"
+    count ??= 1; // If count is null, assign 1
+
+    for (int i = 0; i < count; i++)
+    {
+        Console.WriteLine(name);
+    }
+}
 
 /* Function Calls */
-PrintName();                                              
+/*PrintName();                                              
 int fiveYearsAgo = GetFiveYearsAgo();                       
 Console.WriteLine($"Five Years Ago was: {fiveYearsAgo}");
 
@@ -45,3 +69,14 @@ Console.WriteLine("Enter A Year");
 int pastYear = Convert.ToInt32(Console.ReadLine());
 int yearsAgo = GetYearDifferenceWithParams(pastYear);
 Console.WriteLine($"This Was {yearsAgo} Years Ago");
+
+Console.WriteLine("Enter Number of Years in the Future or Past");
+int NumberOfYears = Convert.ToInt32(Console.ReadLine());
+var pastYear1 = GetFutureOrPastYear();
+Console.WriteLine($"The Year is: {pastYear1}");
+
+var pastYear2 = GetFutureOrPastYear(NumberOfYears);
+Console.WriteLine($"The Year is: {pastYear2}"); */
+
+PrintNameNullableParam(null, null);
+PrintNameNullableParam("Jesus Salinas", 5);
