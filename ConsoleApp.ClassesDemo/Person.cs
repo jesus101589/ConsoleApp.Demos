5b3336094f1f9446e6ef679de42a15a65292ec08
Jesus Salinas <34974398+jesus101589@users.noreply.github.com>
2025-07-29 01:31:31 -0500

    Created a class and will continue in a couple of days. I have an exam for University at 12:45PM. I need to get some sleep. If anyone on the hiring team reads this, please give me a chance! I will not let you down and I will bust my ass everyday!

---
+++ ConsoleApp.ClassesDemo/Person.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Define a class
+public class Person
+{
+    // Properties/Data members
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public DateOnly DateOfBirth { get; set; }
+
+    public void PrintFullName()
+    {
+        Console.WriteLine($"Full Name: {FirstName} {LastName}");
+    }
+}
